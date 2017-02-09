2a0f56aedc4eff8b8fedf238626fc4c736f13ade
Michael Gaudet <Michael.G224@gmail.com>
2017-02-08 18:32:18 -0600

    added index and elements

---
+++ Js/js.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.navBar-bg').hide();
+  $(window).scroll(function(){
+  if($(window).scrollTop()>70){
+    console.log($(window).scrollTop());
+    $('.navBar').hide(500);
+    $('.navBar-bg').show(500);
+  }
+else {
+  $('.navBar-bg').hide(500);
+  $('.navBar').show(500);
+  }
+});
+});
